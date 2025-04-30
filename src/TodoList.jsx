7631b8f9c58c9d9c53af04d210f89ef1750747c0
nusuit <huykien283@gmail.com>
2025-04-30 16:07:40 +0700

    update & complete basic todolist app

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,18 +2,31 @@ import React from "react";
 
 class TodoList extends React.Component {
   state = {
-    tasks: "",
+    tasks: [],
+    newTask: "",
   };
 
   handleOnChange = (event) => {
     this.setState({
-      tasks: event.target.value,
+      newTask: event.target.value,
     });
   };
 
   handleOnClick = (event) => {
     event.preventDefault();
     console.log(">>> clicked");
+    this.setState({
+      tasks: [...this.state.tasks, this.state.newTask],
+      newTask: "",
+    });
+  };
+
+  handleRemove = (task) => {
+    let currentTask = this.state.tasks; //use let because const cannot change value
+    currentTask = currentTask.filter((item) => item.id !== task.id);
+    this.setState({
+      tasks: currentTask,
+    });
   };
   render() {
     const username = localStorage.getItem("username") || "Guest";
@@ -21,10 +34,19 @@ class TodoList extends React.Component {
       <>
         <h2>Good morning {username} </h2>
         <h3>What do your main goal for today</h3>
-        <input type="text" onChange={(event) => this.handleOnChange(event)} />
+        <input
+          type="text"
+          value={this.state.newTask}
+          onChange={(event) => this.handleOnChange(event)}
+        />
         <div>
           <ol>
-            <li>your tasks: {this.state.tasks}</li>
+            {this.state.tasks.map((task, index) => (
+              <li key={index}>
+                {task}
+                <span onClick={(task) => this.handleRemove(task)}> x</span>
+              </li>
+            ))}
           </ol>
         </div>
         <input
